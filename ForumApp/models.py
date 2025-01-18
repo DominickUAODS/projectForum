e65c8878b1c39875848ce5a968a677a8779519be
VD <VD@VD>
2025-01-18 16:44:36 +0200

    custom user data

--- ForumApp/models.py
+++ ForumApp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    birthDate = models.DateField(null=True, verbose_name="Дата рождения", help_text="Укажите дату рождения")
